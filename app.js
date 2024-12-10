4635a84573942d98c926051b4cfe3508ecb7d7cf
danielszago29 <133285013+danielszago29@users.noreply.github.com>
2024-12-10 15:37:05 -0300

    mobile

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 
 //window.alert('Bem vindo')
+function row (){
 
+document.addEventListener("DOMContentLoaded", ()=>{
+    
+    const rowBody = document.querySelector(".row-body");
+    const allItems = rowBody.querySelector(".item-row-body");
+})
+
+}
